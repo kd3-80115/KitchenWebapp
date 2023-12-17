989d5f6d33497b58bbe5db3a52fa5339b8c766ed
kd3-80115 <amkadnan89@gmail.com>
2023-12-17 16:36:16 +0530

    project created

---
+++ src/components/Launcher.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Launcher() {
+    return (<div>
+        <hr></hr>
+        <hr></hr>
+    </div>);
+}
+
+export default Launcher;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Launcher from './components/Launcher';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Launcher></Launcher>);
